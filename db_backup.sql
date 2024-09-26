8294f81adecc1808391ffeeb0fc0daaf7bcc5593
Sean Graham <seangraham006@gmail.com>
2024-09-26 05:39:27 +0000

    added years and rounds successfully

--- db_backup.sql
+++ db_backup.sql
@@ -17,21 +17,30 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 SET default_tablespace = '';
+
 SET default_table_access_method = heap;
+
 --
 -- Name: games; Type: TABLE; Schema: public; Owner: freecodecamp
 --
+
 CREATE TABLE public.games (
     game_id integer NOT NULL,
     winner_id integer NOT NULL,
     opponent_id integer NOT NULL,
     winner_goals integer NOT NULL,
-    opponent_goals integer NOT NULL
+    opponent_goals integer NOT NULL,
+    year integer NOT NULL,
+    round character varying(50) NOT NULL
 );
+
+
 ALTER TABLE public.games OWNER TO freecodecamp;
+
 --
 -- Name: games_game_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
 --
+
 CREATE SEQUENCE public.games_game_id_seq
     AS integer
     START WITH 1
@@ -39,22 +48,33 @@ CREATE SEQUENCE public.games_game_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
+
+
 ALTER TABLE public.games_game_id_seq OWNER TO freecodecamp;
+
 --
 -- Name: games_game_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
 --
+
 ALTER SEQUENCE public.games_game_id_seq OWNED BY public.games.game_id;
+
+
 --
 -- Name: teams; Type: TABLE; Schema: public; Owner: freecodecamp
 --
+
 CREATE TABLE public.teams (
     team_id integer NOT NULL,
     name character varying(50)
 );
+
+
 ALTER TABLE public.teams OWNER TO freecodecamp;
+
 --
 -- Name: teams_team_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
 --
+
 CREATE SEQUENCE public.teams_team_id_seq
     AS integer
     START WITH 1
@@ -62,61 +82,158 @@ CREATE SEQUENCE public.teams_team_id_seq
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
+
+
 ALTER TABLE public.teams_team_id_seq OWNER TO freecodecamp;
+
 --
 -- Name: teams_team_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
 --
+
 ALTER SEQUENCE public.teams_team_id_seq OWNED BY public.teams.team_id;
+
+
 --
 -- Name: games game_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
 --
+
 ALTER TABLE ONLY public.games ALTER COLUMN game_id SET DEFAULT nextval('public.games_game_id_seq'::regclass);
+
+
 --
 -- Name: teams team_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
 --
+
 ALTER TABLE ONLY public.teams ALTER COLUMN team_id SET DEFAULT nextval('public.teams_team_id_seq'::regclass);
+
+
 --
 -- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
-COPY public.games (game_id, winner_id, opponent_id, winner_goals, opponent_goals) FROM stdin;
+
+COPY public.games (game_id, winner_id, opponent_id, winner_goals, opponent_goals, year, round) FROM stdin;
+1	1	2	4	2	2018	Final
+2	3	4	2	0	2018	Third Place
+3	2	4	2	1	2018	Semi-Final
+4	1	3	1	0	2018	Semi-Final
+5	2	5	3	2	2018	Quarter-Final
+6	4	6	2	0	2018	Quarter-Final
+7	3	7	2	1	2018	Quarter-Final
+8	1	8	2	0	2018	Quarter-Final
+9	4	9	2	1	2018	Eighth-Final
+10	6	10	1	0	2018	Eighth-Final
+11	3	11	3	2	2018	Eighth-Final
+12	7	12	2	0	2018	Eighth-Final
+13	2	13	2	1	2018	Eighth-Final
+14	5	14	2	1	2018	Eighth-Final
+15	8	15	2	1	2018	Eighth-Final
+16	1	16	4	3	2018	Eighth-Final
+17	17	16	1	0	2014	Final
+18	18	7	3	0	2014	Third Place
+19	16	18	1	0	2014	Semi-Final
+20	17	7	7	1	2014	Semi-Final
+21	18	19	1	0	2014	Quarter-Final
+22	16	3	1	0	2014	Quarter-Final
+23	7	9	2	1	2014	Quarter-Final
+24	17	1	1	0	2014	Quarter-Final
+25	7	20	2	1	2014	Eighth-Final
+26	9	8	2	0	2014	Eighth-Final
+27	1	21	2	0	2014	Eighth-Final
+28	17	22	2	1	2014	Eighth-Final
+29	18	12	2	1	2014	Eighth-Final
+30	19	23	2	1	2014	Eighth-Final
+31	16	10	1	0	2014	Eighth-Final
+32	3	24	2	1	2014	Eighth-Final
 \.
+
+
 --
 -- Data for Name: teams; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
+
 COPY public.teams (team_id, name) FROM stdin;
+1	France
+2	Croatia
+3	Belgium
+4	England
+5	Russia
+6	Sweden
+7	Brazil
+8	Uruguay
+9	Colombia
+10	Switzerland
+11	Japan
+12	Mexico
+13	Denmark
+14	Spain
+15	Portugal
+16	Argentina
+17	Germany
+18	Netherlands
+19	Costa Rica
+20	Chile
+21	Nigeria
+22	Algeria
+23	Greece
+24	United States
 \.
+
+
 --
 -- Name: games_game_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
-SELECT pg_catalog.setval('public.games_game_id_seq', 1, false);
+
+SELECT pg_catalog.setval('public.games_game_id_seq', 32, true);
+
+
 --
 -- Name: teams_team_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
-SELECT pg_catalog.setval('public.teams_team_id_seq', 1, false);
+
+SELECT pg_catalog.setval('public.teams_team_id_seq', 24, true);
+
+
 --
 -- Name: games games_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
 --
+
 ALTER TABLE ONLY public.games
     ADD CONSTRAINT games_pkey PRIMARY KEY (game_id);
+
+
 --
 -- Name: teams teams_name_key; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
 --
+
 ALTER TABLE ONLY public.teams
     ADD CONSTRAINT teams_name_key UNIQUE (name);
+
+
 --
 -- Name: teams teams_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
 --
+
 ALTER TABLE ONLY public.teams
     ADD CONSTRAINT teams_pkey PRIMARY KEY (team_id);
+
+
 --
 -- Name: games games_opponent_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
 --
+
 ALTER TABLE ONLY public.games
     ADD CONSTRAINT games_opponent_id_fkey FOREIGN KEY (opponent_id) REFERENCES public.teams(team_id);
+
+
 --
 -- Name: games games_winner_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
 --
+
 ALTER TABLE ONLY public.games
     ADD CONSTRAINT games_winner_id_fkey FOREIGN KEY (winner_id) REFERENCES public.teams(team_id);
+
+
 --
 -- PostgreSQL database dump complete
+--
+
